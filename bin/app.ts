93af52c5b16dab2e5c103b271779b53ce3532e78
Enrique Alonso Rivera Nevarez <enriqueriveranevarez@gmail.com>
2024-09-11 09:05:19 -0600

    Updated app.ts

--- bin/app.ts
+++ bin/app.ts
@@ -1,27 +1,42 @@
 #!/usr/bin/env node
 import * as cdk from 'aws-cdk-lib';
-import { NetworkStack } from '../lib/stacks/network-stack';
-import { ConfigProvider } from '../lib/utils/config-loader';
+import { YamlReader } from '../lib/utils/yml-reader';
+import { VpcConstruct } from '../lib/constructs/vpc-construct';
 
 const app = new cdk.App();
-
-const configEnv = app.node.tryGetContext('env') || 'dev';
-
-// Initialize the ConfigProvider with the environment
-const configProvider = ConfigProvider.getInstance(configEnv);
-const config = configProvider.getConfig();
+const env = app.node.tryGetContext('env') || 'dev';
+const config = YamlReader.readValue(env);
 
 // Get the AWS account and region from the config
 const awsAccount = config.aws_account;
 const awsRegion = config.aws_region;
 
-// Create the NetworkStack with the correct props and environment
-new NetworkStack(app, `NetworkStack-${configEnv}`, { 
-  env: {
-    account: awsAccount,
-    region: awsRegion
-  },
-  configEnv: configEnv
+const constructMap: { [key: string]: new (scope: cdk.App, id: string, props: any) => cdk.Construct } = {
+  vpc: VpcConstruct,
+  // ...
+};
+
+const createdConstructs: { [key: string]: cdk.Construct } = {};
+
+Object.keys(constructMap).forEach((key) => {
+  const constructConfig = config.get(key)
+
+  if (constructConfig) {
+    const cdkName = `${env}-${key.charAt(0).toUpperCase() + key.slice(1)}`
+
+    const stack = new cdk.Stack(app, `${cdkName}Stack`, {
+      env: {
+        account: awsAccount,
+        region: awsRegion
+      }
+    });
+
+    const ConstructClass = constructMap[key];
+    createdConstructs[key] = new ConstructClass(stack, `${cdkName}Construct`, {
+      config: constructConfig,
+      env: env
+    });
+  }
 });
 
 app.synth();
